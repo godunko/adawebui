98e188ce933ddd2ccbb0829a003be5619ea4d8ee
Vadim Godunko <vgodunko@gmail.com>
2020-02-15 19:18:50 +0300

    Parameterless slots.

---
+++ source/core/web-core-connectables-slots_0-generic_slots.ads
@@ -0,0 +1,74 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2016-2020, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision: 5711 $ $Date: 2017-01-21 21:29:05 +0300 (Sat, 21 Jan 2017) $
+------------------------------------------------------------------------------
+
+generic
+   type Abstract_Object is
+     abstract new Web.Core.Connectables.Connectable_Object with private;
+
+   with procedure Subprogram (Self : in out Abstract_Object) is abstract;
+
+package Web.Core.Connectables.Slots_0.Generic_Slots is
+
+   pragma Preelaborate;
+
+   function To_Slot
+    (Self : in out Abstract_Object'Class) return Slots_0.Slot'Class;
+
+private
+
+   type Slot (Object : not null access Abstract_Object'Class) is
+     new Web.Core.Connectables.Slots_0.Slot with null record;
+
+   overriding function Create_Slot_End
+    (Self : Slot) return not null Slot_End_Access;
+
+   type Slot_End (Object : not null access Abstract_Object'Class) is
+     new Slot_End_0 with null record;
+
+   overriding procedure Invoke (Self : in out Slot_End);
+
+   overriding function Owner
+    (Self : Slot_End) return not null Connectables.Object_Access;
+
+end Web.Core.Connectables.Slots_0.Generic_Slots;
