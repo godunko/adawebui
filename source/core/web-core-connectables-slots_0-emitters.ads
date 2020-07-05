1b2da97296c4acd24b2d7c3f96672cce9028c78b
Vadim Godunko <vgodunko@gmail.com>
2020-07-05 12:46:48 +0300

    Emitters package parameterless signals.

---
+++ source/core/web-core-connectables-slots_0-emitters.ads
@@ -0,0 +1,67 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2016-2020, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision: 5703 $ $Date: 2017-01-20 22:17:20 +0300 (Пт, 20 янв 2017) $
+------------------------------------------------------------------------------
+
+package Web.Core.Connectables.Slots_0.Emitters is
+
+   pragma Preelaborate;
+
+   type Emitter (Owner : not null access Connectable_Object'Class) is
+     limited new Slots_0.Signal with private;
+
+   procedure Emit (Self : in out Emitter'Class);
+
+private
+
+   type Signal_End is new Signal_End_Base with null record;
+
+   procedure Invoke (Self : in out Signal_End'Class);
+
+   type Emitter (Owner : not null access Connectable_Object'Class) is
+     limited new Emitter_Base and Slots_0.Signal with null record;
+
+   overriding procedure Connect
+    (Self : in out Emitter;
+     Slot : Slots_0.Slot'Class);
+
+end Web.Core.Connectables.Slots_0.Emitters;
